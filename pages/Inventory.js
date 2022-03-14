84ca44bcb1b80526b1a8a1c84a6cf0d3bba58175
mayankSH-addweb <mayank@addwebsolution.in>
2022-03-14 19:06:03 +0530

    add inventory

---
+++ pages/Inventory.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ManageSections from "../components/inventory/ManageSections";
+
+const Inventory = () => {
+  return (
+    <div>
+      <ManageSections />
+    </div>
+  );
+};
+
+export default Inventory;
